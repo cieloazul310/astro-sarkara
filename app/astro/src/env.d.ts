cbe16b5c0de582111402479d5266d3d305ae2ee0
cieloazul310 <cieloazul310@gmail.com>
2023-10-06 17:41:03 +0900

    feat(components): add image url resolution for ogimage

--- app/astro/src/env.d.ts
+++ app/astro/src/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
 interface ImportMetaEnv {
   readonly MICROCMS_SERVICE_DOMAIN: string;
   readonly MICROCMS_API_KEY: string;
