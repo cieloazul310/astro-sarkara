75a28a7b49bcf87147597280b3c54af49b7a2190
cieloazul310 <cieloazul310@gmail.com>
2023-10-08 08:51:10 +0900

    feat(core): add `defineSarkaraConfig`

--- packages/core/src/preset.ts
+++ packages/core/src/preset.ts
@@ -1,8 +1,24 @@
 import * as path from "path";
-import { definePreset, type Preset } from "@pandacss/dev";
+import {
+  definePreset,
+  defineConfig,
+  type Preset,
+  type Config,
+} from "@pandacss/dev";
 import sarkaraPresetBase from "@cieloazul310/panda-preset-sarkara-base";
 import sarkaraComponentsPreset from "@cieloazul310/astro-sarkara-components/preset";
 import { definePalette, type SarkaraPaletteOptions } from "./definePalette";
+
+export const astroComponentsPaths = [
+  "@cieloazul310/astro-sarkara-components",
+  "@cieloazul310/astro-sarkara-layout",
+  "@cieloazul310/astro-sarkara-article-classes",
+].map((components) =>
+  path.resolve(
+    path.dirname(require.resolve(components)),
+    "**/*.{js,ts,jsx,tsx,astro}",
+  ),
+);
 
 export type CreateSarkaraPresetOptions = Preset & {
   palette: SarkaraPaletteOptions;
@@ -27,13 +43,34 @@ export function createSarkaraPreset({
   });
 }
 
-export const astroComponentsPaths = [
-  "@cieloazul310/astro-sarkara-components",
-  "@cieloazul310/astro-sarkara-layout",
-  "@cieloazul310/astro-sarkara-article-classes",
-].map((components) =>
-  path.resolve(
-    path.dirname(require.resolve(components)),
-    "**/*.{js,ts,jsx,tsx,astro}",
-  ),
-);
+export function defineSarkaraConfig({
+  palette,
+  ...options
+}: CreateSarkaraPresetOptions & Config): Config {
+  return defineConfig({
+    ...options,
+    preflight: true,
+    presets: [
+      "@pandacss/dev/presets",
+      sarkaraPresetBase,
+      sarkaraComponentsPreset,
+      ...(options?.presets ?? []),
+    ],
+    include: [...astroComponentsPaths, ...(options?.include ?? [])],
+    outdir: "@cieloazul310/sarkara-css",
+    emitPackage: true,
+    theme: {
+      ...options?.theme,
+      extend: {
+        ...options?.theme?.extend,
+        semanticTokens: {
+          ...options?.theme?.extend?.semanticTokens,
+          colors: {
+            ...definePalette(palette),
+            ...options?.theme?.extend?.semanticTokens?.colors,
+          },
+        },
+      },
+    },
+  });
+}
