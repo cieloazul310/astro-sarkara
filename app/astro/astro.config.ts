d72ee723f59b9e010a46cb51f79f17a403cffa1b
cieloazul310 <cieloazul310@gmail.com>
2023-10-10 18:54:54 +0900

    feat(layout): add slots `top-of-head`, `bottom-of-head`, `top-of-body` and `bottom-of-body`

--- app/astro/astro.config.ts
+++ app/astro/astro.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
 import pandacss from "@pandacss/astro";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import rehypeClassNames from "rehype-class-names";
 import { rehypeClassNamesOptions } from "@cieloazul310/astro-sarkara/classes";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), mdx()],
+  integrations: [
+    pandacss(),
+    mdx(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     rehypePlugins: [[rehypeClassNames, rehypeClassNamesOptions]],
   },
