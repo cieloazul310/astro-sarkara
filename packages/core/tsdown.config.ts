0e324e1410534a972fa271bfd1f1228f8d0e199a
cieloazul310 <cieloazul310@gmail.com>
2026-07-03 17:45:04 +0900

    chore: tiny fix

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -2,15 +2,9 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/preset.ts", "src/article-classes.ts"],
-  external: [
-    "@pandacss/dev",
-    "@cieloazul310/astro-sarkara-article-classes",
-    "@cieloazul310/panda-preset-sarkara-base",
-    "@cieloazul310/astro-sarkara-components",
-    "@cieloazul310/astro-sarkara-layout",
-    "@cieloazul310/astro-sarkara-utils",
-    "styled-system",
-  ],
+  deps: {
+    neverBundle: [/^@pandacss\//, /^@cieloazul310\//, "styled-system"],
+  },
   format: {
     esm: {
       target: ["esnext"],
